9b47b3d0e4ef439226bfacabb48199df3992d691
ineedtodosomething <141281958+ineedtodosomething@users.noreply.github.com>
2023-10-04 18:22:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ class Extension {
     }
     getInfo() {
         return {
-            
+            "id": "darkBlack",
+            "name": "DarkBlack",
+            "blocks": [
+                {
+                        
+                }
+            ]
         }
     }
+    /* TODO: 
+        add blocks 
+        add stuff
+        add polish
+    */
 }
 Scratch.extensions.register(new Extension())
